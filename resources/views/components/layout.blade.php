3a828d0e5bce04165c7db5e9232a7f622b63ed38
Knight Tan <knight@reworklabs.com>
2025-01-17 23:10:33 +0800

    added folio for front end . setup blog index

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Example Theme</title>
+    @livewireStyles
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="flex items-center justify-center w-screen h-screen">
+    {{ $slot}}
+    @livewireScripts
+</body>
+</html>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,35 @@
+<?php
+$posts = \App\Models\Post::orderBy("created_at", "DESC")->paginate(6); ?>
+
+<x-layout>
+    <div class="bg-white py-24 sm:py-32">
+      <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <div class="mx-auto max-w-2xl lg:mx-0">
+          <h2 class="text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">From the blog</h2>
+          <p class="mt-2 text-lg/8 text-gray-600">Learn how to grow your business with our expert advice.</p>
+        </div>
+        <div class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+        @foreach ($posts as $post)
+          <article class="flex max-w-xl flex-col items-start justify-between">
+            <div class="flex items-center gap-x-4 text-xs">
+              <time datetime="2020-03-16" class="text-gray-500">{{ $post->published_at->format('M d, Y') }}</time>
+              <a href="#" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Category</a>
+            </div>
+            <div class="group relative">
+              <h3 class="mt-3 text-lg/6 font-semibold text-gray-900 group-hover:text-gray-600">
+                <a href="#">
+                  <span class="absolute inset-0"></span>
+                  {{ $post->title}}
+                </a>
+              </h3>
+              <p class="mt-5 line-clamp-3 text-sm/6 text-gray-600">{{ substr(strip_tags($post->content), 0, 110) }}</p>
+            </div>
+
+          </article>
+          @endforeach
+          <!-- More posts... -->
+        </div>
+      </div>
+    </div>
+
+</x-layout>
