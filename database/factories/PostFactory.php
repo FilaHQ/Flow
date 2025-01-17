a5a457b38974976d2a999a533dbf0061727a53b6
Knight Tan <knight@reworklabs.com>
2025-01-17 20:31:51 +0800

    build factory to test post

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title = fake()->words(3, true);
+        return [
+            "title" => $title,
+            "content" => fake()->paragraph(),
+            "slug" => trim(\Str::slug($title)),
+            "published_at" => now(),
+        ];
+    }
+}
