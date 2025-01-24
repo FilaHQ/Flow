49c097b0006446cba61bf4a4a2f1df0261990da0
Knight Tan <knight@reworklabs.com>
2025-01-25 06:59:23 +0800

    remove folio, put custom theme idea aside first

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,7 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\FrontendPanelProvider::class,
     App\Providers\FolioServiceProvider::class,
+    App\Providers\VoltServiceProvider::class,
 ];
--- resources/views/pages/[Post-slug].blade.php
+++
@@ -1,9 +0,0 @@
-<x-layout>
-<div class="bg-white px-6 py-32 lg:px-8">
-  <div class="mx-auto max-w-3xl text-base/7 text-gray-700">
-    <p class="text-base/7 font-semibold text-indigo-600">{{ $post->published_at->format('M d, Y') }}</p>
-    <h1 class="mt-2 text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">{{ $post->title}}</h1>
-    <p class="mt-6 text-xl/8">{{ $post->content}}</p>
-  </div>
-</div>
-</x-layout>
