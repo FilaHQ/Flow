80df278955de1c7964efa97063ea0abf50134179
Knight Tan <knight@reworklabs.com>
2025-01-25 23:18:27 +0800

    remove both volt n folio serivce for now

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,6 +4,4 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\Filament\FrontendPanelProvider::class,
-    App\Providers\FolioServiceProvider::class,
-    App\Providers\VoltServiceProvider::class,
 ];
